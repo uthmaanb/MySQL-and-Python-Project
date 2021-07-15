39b9fa79ba7726f12dfa483cdc295d3e35b17985
ウスマーン <82204694+uthmaanb@users.noreply.github.com>
2021-07-15 13:04:12 +0200

    'update'

--- admin.py
+++ admin.py
@@ -1,21 +1,45 @@
 from tkinter import *
-import mysql
-import mysql.connector
-from tkinter import messagebox
+# import mysql
+import mysql.connector as mysql
+from tkinter import messagebox, Text
 from datetime import datetime
 
 root = Tk()
 # widget features & style
-root.geometry("450x400")
-root.title("MySQL (Uthmaan Breda)")
-root.config(bg="red")
+root.geometry("600x350")
+root.title("MySQL admin (Uthmaan Breda)")
+root.config(bg="#ffffff")
 
-Label(root, text='Username:', bg="red", font="poppins 10 bold").place(x=30, y=50)
-user_ent = Entry(root)
-user_ent.place(x=100, y=50)
-Label(root, text='Password:', bg="red", font="poppins 10 bold").place(x=30, y=100)
-pass_ent = Entry(root)
-pass_ent.place(x=100, y=100)
+frame = Frame(root, width=350, height=400, bg='#346ab3')
+frame.place(x=0, y=0)
+Label(frame, text='Life Choices Academy', height=2, bg="#346ab3", fg='white',
+      font=('poppins sans serif', 20, 'italic')).place(x=10, y=50)
+
+name_check = False
+pass_check = False
+
+
+def name_text(event):
+    user_ent.configure(state=NORMAL)
+    user_ent.delete(0, END)
+    name_check = True
+
+
+def pass_text(event):
+    pass_ent.configure(state=NORMAL)
+    pass_ent.delete(0, END)
+    pass_check = True
+
+
+Label(root, text='Admin Login', height=2, bg="#ffffff", font=('poppins', 15, 'italic')).place(x=390, y=20)
+user_ent = Entry(root, font=('Poppins sans serif', 27), width=10)
+user_ent.insert(0, 'username')
+user_ent.bind('<Button-1>', name_text)
+user_ent.place(x=370, y=93)
+pass_ent = Entry(root, font=('Poppins sans serif', 27), width=10)
+pass_ent.insert(0, 'password')
+pass_ent.bind('<Button-1>', pass_text)
+pass_ent.place(x=370, y=136)
 
 
 def back():
@@ -28,41 +52,44 @@ def kill():
 
 
 def login():
-    date = datetime.now().date().strftime("%Y-%m-%d")
-    time = datetime.now().time().strftime('%H:%M:%S')
-    mydb = mysql.connector.connect(user='root',
-                                   password='Themainp1zza!',
-                                   host='127.0.0.1',
-                                   database='login_lc',
-                                   auth_plugin='mysql_native_password',
-                                   buffered=TRUE)
-    mycursor = mydb.cursor()
+    try:
+        mydb = mysql.connect(user='root',
+                             password='Themainp1zza!',
+                             host='127.0.0.1',
+                             database='login_lc',
+                             auth_plugin='mysql_native_password',
+                             buffered=True)
 
-    if user_ent.get() == '' or pass_ent.get() == '':
-        messagebox.showerror('error', 'fill in all fields', parent=root)
+        cursor = mydb.cursor()
+        cursor.execute("Select * from users")
 
-    mycursor.execute('Select username, password, role from users')
-    for i in mycursor:
-        if user_ent.get() == i[0] and pass_ent.get() == i[1] and i[2] == "admin":
+        date = datetime.now().date().strftime("%Y-%m-%d")
+        time = datetime.now().time().strftime('%H:%M:%S')
 
-            sql = "INSERT INTO login (username, date_login, login) VALUES (%s, %s, %s)"
-            val = (user_ent.get(), date, time)
+        if user_ent.get() == "" or pass_ent.get() == "":
+            messagebox.showerror("Error!", "Fill in all fields")
+        for i in cursor:
+            if user_ent.get() == i[2] and pass_ent.get() == i[3] and i[8] == 'admin':
+                sql = "INSERT INTO login (username, date_login, login) VALUES (%s, %s, %s)"
+                values = (user_ent.get(), date, time)
+                cursor.execute(sql, values)
+                mydb.commit()
+                messagebox.showinfo("STATUS", "Access Granted. Welcome " + str(user_ent.get()))
+                root.destroy()
+                import treeveiw
+        else:
+            messagebox.showerror("ERROR", "Access Denied")
 
-            mycursor.execute(sql, val)
-            mydb.commit()
-
-            messagebox.showinfo('Success', 'You are logged in')
-            root.destroy()
-            import treeveiw
-
-    else:
-        messagebox.showerror('ERROR!!!!!!!!!!!', 'Incorrect credentials')
+    except mysql.Error as err:  # This except statement will catch all mysql errors
+        messagebox.showerror("Error", "Something went wrong: " + str(err))
 
 
-Button(root, text='Login', command=login, bg="red", font="poppins 10 bold", border="5").place(x=80, y=150)
-Button(root, text='return to user login', command=back, bg="red", font="poppins 10 bold", border="5").place(x=180,
-                                                                                                            y=150)
-Button(root, text='exit', command=kill, bg="red", font="poppins 10 bold", border="5").place(x=80, y=230)
+Button(root, text='Log in', command=login, bg="#346ab3", fg='white', width=25, height=2, font="poppins 10 bold",
+       border="0").place(x=370, y=220)
+Button(root, text='User login', command=back, bg="#346ab3", fg='white', width=11, height=2, font="poppins 10 bold",
+       border="0").place(x=370, y=280)
+Button(root, text='Exit', command=kill, bg="#346ab3", fg='white', width=11, height=2, font="poppins 10 bold",
+       border="0").place(x=483, y=280)
 
 
 root.mainloop()  # continuously runs program in window
--- logout.py
+++ logout.py
@@ -5,16 +5,39 @@ from datetime import datetime
 
 root = Tk()
 # widget features & style
-root.geometry("450x400")
+root.geometry("600x350")
 root.title("MySQL logout (Uthmaan Breda)")
-root.config(bg="red")
+root.config(bg="#ffffff")
 
-Label(root, text='Username:', bg="red", font="poppins 10 bold").place(x=30, y=50)
-user_ent = Entry(root)
-user_ent.place(x=100, y=50)
-Label(root, text='Password:', bg="red", font="poppins 10 bold").place(x=30, y=100)
-pass_ent = Entry(root)
-pass_ent.place(x=100, y=100)
+frame = Frame(root, width=350, height=400, bg='#346ab3')
+frame.place(x=0, y=0)
+Label(frame, text='Life Choices Academy', height=2, bg="#346ab3", fg='white', font=('poppins sans serif', 20, 'italic')).place(x=10, y=50)
+
+name_check = False
+pass_check = False
+
+
+def name_text(event):
+    user_ent.configure(state=NORMAL)
+    user_ent.delete(0, END)
+    name_check = True
+
+
+def pass_text(event):
+    pass_ent.configure(state=NORMAL)
+    pass_ent.delete(0, END)
+    pass_check = True
+
+
+Label(root, text='Enjoy the rest of\nyour day.', height=2, bg="#ffffff", font=('poppins', 15, 'italic')).place(x=390, y=20)
+user_ent = Entry(root, font=('Poppins sans serif', 27), width=10)
+user_ent.insert(0, 'username')
+user_ent.bind('<Button-1>', name_text)
+user_ent.place(x=370, y=93)
+pass_ent = Entry(root, font=('Poppins sans serif', 27), width=10)
+pass_ent.insert(0, 'password')
+pass_ent.bind('<Button-1>', pass_text)
+pass_ent.place(x=370, y=136)
 
 
 def logout():
@@ -43,7 +66,8 @@ def logout():
         messagebox.showerror('ERROR!!!!!!!!!!!', 'Incorrect credentials')
 
 
-Button(root, text='Logout', command=logout, bg="red", font="poppins 10 bold", border="5").place(x=80, y=150)
+Button(root, text='Log out', command=logout, bg="#346ab3", fg='white', width=25, height=2, font="poppins 10 bold",
+       border="0").place(x=370, y=220)
 
 root.mainloop()  # continuously runs program in window
 
